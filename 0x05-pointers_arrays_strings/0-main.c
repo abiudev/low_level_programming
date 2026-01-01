4a3e7b03ed555b3c315cf2648387205e707b0a70
Dev Abiud <136119623+abiudev@users.noreply.github.com>
2026-01-02 01:37:54 +0300

    Refactor main function for reset_to_98 test
    Updated main function to improve output clarity.

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * main - Entry point to test the reset_to_98 function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int n;
+    int n = 402;
 
-    n = 402;
-    printf("n=%d\n", n);
+    printf("Before: n = %d\n", n);
     reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+    printf("After : n = %d\n", n);
+
+    return 0;
 }
